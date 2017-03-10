34d93957dd82ad21e8d88d017b79765b416ac1d4
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-03-09 22:42:15 -0500

    added 'hamburger'  style responsive menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('sass', ()=> {
   gulp.src('*.scss')
--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,21 @@
 $(document).ready( () => {
+    // immediate state on page load.   
+    $('.fa-bars').removeClass('active');
+    $('#menu').addClass('hidden');
 
-    $(".user-info").keyup( (event) => { 
+    $('.user-info').keyup( (event) => { 
         if (event.keyCode === 13) {
-             console.log("pressed enter!")
+             console.log('pressed enter!')
         }
+    })
+
+    // display max character limit
+    $( ).on( 'keypress', ()=> {
+        
+    })
+
+    $('.fa-bars').click( ()=> {
+        $('.fa-bars').toggleClass('active');
+        $('#menu').toggleClass('hidden');
     })
 });
